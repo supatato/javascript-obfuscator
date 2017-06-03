e07702754c1ad7ab299ff8bb136742d37bd30b2a
sanex3339 <sanex3339@yandex.ru>
2017-06-03 15:15:55 +0300

    tests refactoring: utils

--- test/unit-tests/utils/crypt-utils/CryptUtils.spec.ts
+++ test/unit-tests/utils/crypt-utils/CryptUtils.spec.ts
@@ -4,40 +4,77 @@ import { CryptUtils } from '../../../../src/utils/CryptUtils';
 
 describe('CryptUtils', () => {
     describe('btoa (string: string): string', () => {
+        const expectedString: string = 'c3RyaW5n';
+
+        let string: string;
+
+        before(() => {
+            string = CryptUtils.btoa('string');
+        });
+
         it('should create a base-64 encoded string from a given string', () => {
-            assert.equal(CryptUtils.btoa('string'), 'c3RyaW5n');
+            assert.equal(string, expectedString);
         });
     });
 
     describe('hideString (str: string, length: number): [string, string]', () => {
-        let original1: string = 'example.com',
-            str1: string,
-            diff: string;
+        const originalString: string = 'example.com';
+        const hiddenStringLength: number = 30;
+
+        let hiddenString: string,
+            diffString: string;
 
         before(() => {
-            [str1, diff] = CryptUtils.hideString(original1, 30);
+            [hiddenString, diffString] = CryptUtils.hideString(originalString, hiddenStringLength);
         });
 
-        it('should return a string with the original string within', () => {
-            assert.isTrue(str1.length > original1.length);
-            assert.equal(str1.replace(new RegExp('[' + diff + ']', 'g'), ''), original1);
+        describe('hidden string length check', () => {
+            let originalStringActualLength: number,
+                hiddenStringActualLength: number;
+
+            before(() => {
+                originalStringActualLength = originalString.length;
+                hiddenStringActualLength = hiddenString.length;
+            });
+
+            it('should create hidden string with length equal or bigger than given length', () => {
+                assert.isTrue(hiddenStringActualLength > hiddenStringLength);
+            });
         });
 
+        describe('hidden string content', () => {
+            let hiddenStringWithoutDiff: string;
+
+            before(() => {
+                const regExp: RegExp = new RegExp(`[${diffString}]`, 'g');
+
+                hiddenStringWithoutDiff = hiddenString.replace(regExp, '');
+            });
+
+            it('should return a hidden string with the original string within', () => {
+                assert.equal(hiddenStringWithoutDiff, originalString);
+            });
+        });
     });
 
     describe('rc4 (string: string, key: string): string', () => {
-        it('should encode string using the rc4 algorithm', () => {
-            const string: string = 'test';
-            const key: string = 'key';
+        const string: string = 'test';
+        const key: string = 'key';
 
-            assert.notEqual(CryptUtils.rc4(string, key), string);
+        let encodedString: string,
+            decodedString: string;
+
+        before(() => {
+            encodedString = CryptUtils.rc4(string, key);
+            decodedString = CryptUtils.rc4(encodedString, key);
+        });
+
+        it('should encode string using the rc4 algorithm', () => {
+            assert.notEqual(encodedString, string);
         });
 
         it('should encode and successfully decode string using the rc4 algorithm', () => {
-            const string: string = 'test';
-            const key: string = 'key';
-
-            assert.equal(CryptUtils.rc4(CryptUtils.rc4(string, key), key), string);
+            assert.equal(decodedString, string);
         });
     });
 });
